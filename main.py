7484526b5a9e6a2e55383d3ba25cd53440666536
Adrien <adrien.dalbeigue@cpe.fr>
2021-12-16 08:41:40 +0100

    debut

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 """-------------------------------------------SPACE-INVADERS--------------------------------------------------------"""
+from tkinter import *
+
+
+myWindow = Tk()
+labelTitre = Label(myWindow, text="SPACE_INVADRES", fg="black")
+labelTitre.pack()
+
+boutonQuitter = Button(myWindow, text="Quitter", fg='black', command= myWindow.destroy)
+boutonQuitter.pack(side='top')
 
 
 
-test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+myWindow.mainloop()
+
+
+
+
+
