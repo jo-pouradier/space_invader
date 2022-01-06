f901f9a4d09891aa026936c14240fb2d090a506e
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2022-01-06 14:38:26 +0100

    nouvel classe world

--- classes.py
+++ classes.py
@@ -2,10 +2,11 @@ from random import *
 import tkinter as tk
 import time
 from PIL import Image, ImageTk
+import os, sys
 
 
 class main_view(tk.Frame):
-    '''
+    """
     main_view
     dev: Joseph
     date: 2021-12-16 09:54:25
@@ -15,54 +16,70 @@ class main_view(tk.Frame):
 
     Parametres:
         tk [tk] : cree une frame tkinter dans la page principale
-    '''
+    """
 
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
 
         # frame pour afficher les info du jeu : nbr de vie, score, boutons...
         info_frame = tk.Frame(self)
-        info_frame.grid(row=0, column=0, sticky='nsew')
-        info_frame.configure(highlightbackground='red', highlightthickness=2)
+        info_frame.grid(row=0, column=0, sticky="nsew")
+        info_frame.configure(highlightbackground="red", highlightthickness=2)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
 
         # canvas qui aura toutes les entitées du jeu.
         self.cv = tk.Canvas(self)
-        self.cv.configure(highlightbackground='blue',
-                          highlightthickness=2)
-        self.cv.grid(row=1, column=0, sticky='nsew')
+        self.cv.configure(highlightbackground="blue", highlightthickness=2)
+        self.cv.grid(row=1, column=0, sticky="nsew")
 
         bouton_quitter = tk.Button(
-            info_frame, text='Quitter', fg='black', command=self.master.destroy)
-        bouton_quitter.grid(row=0, column=4, sticky='ensw')
+            info_frame, text="Quitter", fg="black", command=self.master.destroy
+        )
+        bouton_quitter.grid(row=0, column=4, sticky="ensw")
         bouton_new_game = tk.Button(
-            info_frame, text='New Game', fg='black', command=None)
-        bouton_new_game.grid(row=0, column=3, sticky='ensw')
+            info_frame, text="New Game", fg="black", command=None
+        )
+        bouton_new_game.grid(row=0, column=3, sticky="ensw")
 
         # ajout d'un boutton menu pour choisir le background
-        background_menu = tk.Menubutton(
-            info_frame, text='Background', fg='black')
+        background_menu = tk.Menubutton(info_frame, text="Background", fg="black")
         background_menu.menu = tk.Menu(background_menu)
-        background_menu['menu'] = background_menu.menu
+        background_menu["menu"] = background_menu.menu
         background_menu.menu.add_command(
-            label='Terre', command=lambda: [self.new_background('images/background_space_1.png'),
-                                            self.cv.tag_lower('background')])
+            label="Terre",
+            command=lambda: [
+                self.new_background("images/background_space_1.png"),
+                self.cv.tag_lower("background"),
+            ],
+        )
         background_menu.menu.add_command(
-            label='Terre 2', command=lambda: [self.new_background('images/background_space_2.png'),
-                                              self.cv.tag_lower('background')])
+            label="Terre 2",
+            command=lambda: [
+                self.new_background("images/background_space_2.png"),
+                self.cv.tag_lower("background"),
+            ],
+        )
         background_menu.menu.add_command(
-            label='Galaxy', command=lambda: [self.new_background('images/background_space_3.png'),
-                                             self.cv.tag_lower('background')])
+            label="Galaxy",
+            command=lambda: [
+                self.new_background("images/background_space_3.png"),
+                self.cv.tag_lower("background"),
+            ],
+        )
         background_menu.menu.add_command(
-            label='Voie Lactée', command=lambda: [self.new_background('images/background_space_4.png'),
-                                                  self.cv.tag_lower('background')])
-        background_menu.grid(row=0, column=2, sticky='esw')
+            label="Voie Lactée",
+            command=lambda: [
+                self.new_background("images/background_space_4.png"),
+                self.cv.tag_lower("background"),
+            ],
+        )
+        background_menu.grid(row=0, column=2, sticky="esw")
 
         self.centrage()
 
     def centrage(self):
-        '''
+        """
         centrage
         dev: Joseph
         date: 2021-11-27 20:54:14
@@ -72,22 +89,27 @@ class main_view(tk.Frame):
 
         Parametres:
             self [Frame] : une frame tkinter
-        '''
-        self.sizeX = self.master.winfo_screenwidth()*0.9
-        self.sizeY = self.master.winfo_screenheight()*0.9
-        positionX = self.master.winfo_screenwidth()/2-(self.sizeX)/2
-        positionY = self.master.winfo_screenheight()/2-(self.sizeY)/2
+        """
+        self.sizeX = self.master.winfo_screenwidth() * 0.9
+        self.sizeY = self.master.winfo_screenheight() * 0.9
+        positionX = self.master.winfo_screenwidth() / 2 - (self.sizeX) / 2
+        positionY = self.master.winfo_screenheight() / 2 - (self.sizeY) / 2
         self.master.geometry(
-            "{}x{}+{}+{}".format(int(self.sizeX), int(self.sizeY), int(positionX), int(positionY)))
+            "{}x{}+{}+{}".format(
+                int(self.sizeX), int(self.sizeY), int(positionX), int(positionY)
+            )
+        )
 
     def new_background(self, img):
         self.background_image = img
         self.space_image = Image.open(img)
         self.space_image = self.space_image.resize(
-            (self.cv.winfo_width(), self.cv.winfo_height()), Image.ANTIALIAS)
+            (self.cv.winfo_width(), self.cv.winfo_height()), Image.ANTIALIAS
+        )
         self.background_resize = ImageTk.PhotoImage(self.space_image)
         self.background = self.cv.create_image(
-            0, 0, image=self.background_resize, anchor='nw', tag='background')
+            0, 0, image=self.background_resize, anchor="nw", tag="background"
+        )
 
 
 # pas sur qu'elle soit utile
@@ -114,22 +136,93 @@ class main_view(tk.Frame):
 # Sp_Inv = Space_invaders("myWindow", "player", 300, 300)  # initialisation test
 
 
-class Entity():
-    '''
+class World:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.player = Player(
+            "player",
+            lives=3,
+            img="images/vaisseau_player.png",
+            position=[self.canvas.winfo_width() / 2, self.canvas.winfo_height() * 0.75],
+            canvas=self.canvas,
+        )
+        self.player.create(tag="player")
+
+        # self.monster = Monster(
+        #     "monster",
+        #     1,
+        #     self.canvas,
+        #     position=[self.canvas.winfo_width() / 4, 70],
+        #     img="images/vaisseau_enemy_3.png",
+        # )
+        # self.monster.create(tag="monster")
+        # self.canvas.tag_raise("monster")
+        # self.monster.deplacement_monstre()
+
+        # self.player.lives_minus(self.monster)
+        # self.monster.lives_minus(self.player)
+        self.canvas.focus_set()
+
+    def level_monster(self, lvl):
+        self.lvl = lvl
+        x = self.canvas.winfo_width() / lvl
+        self.list_monster = []
+        for i in range(lvl):
+            monster = Monster(
+                "monster",
+                1,
+                self.canvas,
+                position=[x, 70],
+                img="images/vaisseau_enemy_3.png",
+            )
+            monster.create(tag="monster")
+            self.list_monster.append(monster)
+            x += 150
+        
+    def moove_monsters(self):
+        #verifier celui de droite et celui de gauche pour choisir la direction
+        
+
+    def dead(self):
+        if self.player.lives == 0:
+            self.canvas.delete(self.player.form)
+            self.player.__del__()
+            restart = tk.messagebox.askquestion("Rejouer ?", "Voulez vous rejouer? ")
+            if restart.upper() == "YES":
+                os.execl(sys.executable, sys.executable, *sys.argv)
+            elif restart.upper() == "NO":
+                sys.exit()
+        self.canvas.after(50, self.dead)
+
+    def player_shoot(self):
+        self.player.deplacement_bullet()
+        self.player.suppr_bullet()
+
+    def monster_shoot(self):
+        self.monster.shoot(None)
+        self.monster.deplacement_bullet()
+        self.monster.suppr_bullet()
+
+
+class Entity(World):
+    """
     Entity
     dev: Adrien
     date: 2021-12-16 09:56:36
 
     Description:
     Classe de position des entitées du jeu, regroupe toutes les données essentielles sur une entité du jeu.
-    '''
+    """
 
-    def __init__(self, lives, canvas, speed=1, position=[0, 0], img=""):
+    def __init__(self, name, lives, canvas, speed=1, position=[0, 0], img=""):
         # nombre de vies (3 pour le joueur et a definir pour les enemies)
+        self.name = name
         self.lives = lives
         self.position = position  # position sur la map
         self.damage = 1
-        self.border = 0  # désigne la ligne que le joueur ou le monstre ne peux pas dépasser
+        self.border = (
+            0  # désigne la ligne que le joueur ou le monstre ne peux pas dépasser
+        )
         self.speed = speed
         self.img = img
         self.canvas = canvas
@@ -138,15 +231,16 @@ class Entity():
     def create(self, tag):
         self.photo = tk.PhotoImage(file=self.img)
         self.form = self.canvas.create_image(
-            self.position[0], self.position[1], image=self.photo, tag=tag)
+            self.position[0], self.position[1], image=self.photo, tag=tag
+        )
 
     # je crois pas quelle soit utile...
-    def placement(self, position):  # positionne l'entité dur la map
-        if len(position) == 2 and position[0] >= 0 and position[1] >= 0:
-            self.position[0] = position[0]
-            self.position[1] = position[1]
-        else:
-            self.position.append([0, 0])
+    # def placement(self, position):  # positionne l'entité dur la map
+    #     if len(position) == 2 and position[0] >= 0 and position[1] >= 0:
+    #         self.position[0] = position[0]
+    #         self.position[1] = position[1]
+    #     else:
+    #         self.position.append([0, 0])
 
     # def shoot(self, nb):  # nb=0 pour le player et nb=1 pour les monstres
     #     if nb == 0:
@@ -155,7 +249,7 @@ class Entity():
     #         self.direction_tir = "down"
 
     def shoot(self, event):
-        '''
+        """
         shoot
         dev: Joseph
         date: 2021-12-17 23:31:43
@@ -166,40 +260,65 @@ class Entity():
 
         Parametres:
             event [event] : <space> pour le payer ou rien pour un monstre.
-        '''
-
-        key = event.keysym
+        """
+        try:
+            key = event.keysym
+        except AttributeError:
+            key = None
         # on passe par l'event (<space>) pour le player et par rien pour les monstres.
-        if key == 'space':
-            self.bullets[(self.canvas.create_oval(
-                self.position[0]-5, self.position[1]-5, self.position[0]+5, self.position[1]+5, fill='green', tag='bullet'))] = [self.position[0], self.position[1]]
+        if key == "space":
+            self.bullets[
+                (
+                    self.canvas.create_oval(
+                        self.position[0] - 5,
+                        self.position[1] - 5,
+                        self.position[0] + 5,
+                        self.position[1] + 5,
+                        fill="green",
+                        tag="bullet",
+                    )
+                )
+            ] = [self.position[0], self.position[1]]
 
         else:
-            self.bullets[(self.canvas.create_oval(
-                self.position[0]-5, self.position[1]-5, self.position[0]+5, self.position[1]+5, fill='red'))] = [self.position[0], self.position[1]]
+            self.bullets[
+                (
+                    self.canvas.create_oval(
+                        self.position[0] - 5,
+                        self.position[1] - 5,
+                        self.position[0] + 5,
+                        self.position[1] + 5,
+                        fill="red",
+                    )
+                )
+            ] = [self.position[0], self.position[1]]
 
     def deplacement_bullet(self):
-        '''
+        """
         deplacement_bullet
         dev: Joseph
         date: 2021-12-17 23:34:09
 
         Description:
         En fonction de self (player ou monstre), la balles se deplace vers le haut ou vers le bas.
-        '''
+        """
 
         for bullet in self.bullets.keys():
-            if self.name == 'player':
+            if self.name == "player":
                 self.bullets[bullet][1] -= 5
             else:
                 self.bullets[bullet][1] += 5
             self.canvas.coords(
-                bullet, self.bullets[bullet][0]-5, self.bullets[bullet][1]-5,
-                self.bullets[bullet][0]+5, self.bullets[bullet][1]+5)
+                bullet,
+                self.bullets[bullet][0] - 5,
+                self.bullets[bullet][1] - 5,
+                self.bullets[bullet][0] + 5,
+                self.bullets[bullet][1] + 5,
+            )
         self.canvas.after(20, self.deplacement_bullet)
 
     def suppr_bullet(self):
-        '''
+        """
         suppr_bullet
         dev: Joseph
         date: 2021-12-17 23:35:25
@@ -207,20 +326,50 @@ class Entity():
         Description:
         On parcours la liste des bullets, on enregistres celles qui sortent dee la fenetre,
         puis on les suprimes du canvas et du dictionnaire.
-        '''
+        """
 
         list_suppr = []
         for bullet in self.bullets.keys():
-            if self.bullets[bullet][1] < 0 or self.bullets[bullet][1] > self.canvas.winfo_height():
+            if (
+                self.bullets[bullet][1] < 0
+                or self.bullets[bullet][1] > self.canvas.winfo_height()
+            ):
                 list_suppr.append(bullet)
         for b in list_suppr:
             self.canvas.delete(b)
             self.bullets.pop(b)
         self.canvas.after(20, self.suppr_bullet)
 
+    def lives_minus(self, ennemi):
+        list_suppr = []
+        for b in self.bullets.keys():
+            # print(ennemi.position)
+            if (
+                (ennemi.position[0] - 47.0)
+                <= self.bullets[b][0]
+                <= (ennemi.position[0] + 47.0)
+            ) and (
+                (ennemi.position[1] - 47.0)
+                <= self.bullets[b][1]
+                <= (ennemi.position[1] + 47.0)
+            ):
+                list_suppr.append(b)
+                list_suppr.append(ennemi)
+                ennemi.lives -= 1  # on a la balle en 0 et l'ennemi en 1
+        if list_suppr != []:
+            try:
+                self.canvas.delete(list_suppr[0])
+                self.bullets.pop(list_suppr[0])
+            except IndexError:
+                pass
+        try:
+            self.canvas.after(10, lambda: self.lives_minus(ennemi))
+        except Exception:
+            return
+
 
 class Player(Entity):
-    '''
+    """
     Player
     dev: Adrien
     date: 2021-12-17 23:40:06
@@ -230,10 +379,13 @@ class Player(Entity):
 
     Parametres:
         Entity [class] : voir description de la class
-    '''
+    """
+
+    def __del__(self):
+        print("you lose")
 
     def deplacement_player(self, event):
-        '''
+        """
         deplacement_player
         dev: Adrien et Joseph
         date: 2021-12-17 23:41:49
@@ -243,29 +395,29 @@ class Player(Entity):
 
         Parametres:
             event [event] : les fleches du clavier (up, right,...)
-        '''
+        """
         side = event.keysym
         # print(side)
         if side == "Left":
-            if self.position[0]-self.speed > 0:
+            if self.position[0] - self.speed > 0:
                 self.position[0] -= self.speed
             else:
                 self.position[0] = self.position[0]
 
         if side == "Right":
-            if self.position[0]+self.speed < self.canvas.winfo_width():
+            if self.position[0] + self.speed < self.canvas.winfo_width():
                 self.position[0] += self.speed
             else:
                 self.position[0] = self.position[0]
 
         if side == "Up":
-            if self.position[1]-self.speed > 0+45:
+            if self.position[1] - self.speed > 0 + 45:
                 self.position[1] -= self.speed
             else:
                 self.position[1] = self.position[1]
 
         if side == "Down":
-            if self.position[1]+self.speed < self.canvas.winfo_height():
+            if self.position[1] + self.speed < self.canvas.winfo_height():
                 self.position[1] += self.speed
             else:
                 self.position[1] = self.position[1]
@@ -274,8 +426,7 @@ class Player(Entity):
 
 
 class Monster(Entity):
-
-    def deplacement_monstre(self, direction='r'):   # speed est un nombre de pixels
+    def deplacement_monstre(self, direction="r"):  # speed est un nombre de pixels
         # if self.position[0] != 0:  # on est a x=0 (gauche)
         #     while self.position[0] != self.canvas.winfo_width():
         #         if self.position[0]+self.speed < self.canvas.winfo_width():
@@ -296,26 +447,22 @@ class Monster(Entity):
         # self.canvas.coords(self.form, self.position[0], self.position[1])
         # self.canvas.after(20, self.deplacement_monstre)
 
-        if self.position[0] < self.canvas.winfo_width()-50 and direction == 'r':
+        if self.position[0] < self.canvas.winfo_width() - 50 and direction == "r":
             self.position[0] += self.speed
-        elif self.position[0] >= self.canvas.winfo_width()-50 and direction == 'r':
-            direction = 'l'
-        elif self.position[0] > 0+50 and direction == 'l':
+        elif self.position[0] >= self.canvas.winfo_width() - 50 and direction == "r":
+            direction = "l"
+        elif self.position[0] > 0 + 50 and direction == "l":
             self.position[0] -= self.speed
-        elif self.position[0] <= 0+50 and direction == 'l':
-            direction = 'r'
+        elif self.position[0] <= 0 + 50 and direction == "l":
+            direction = "r"
         self.canvas.coords(self.form, self.position[0], self.position[1])
         self.canvas.after(10, lambda: self.deplacement_monstre(direction))
+
+    def __del__(self):
+        print("a monster has been deleted")
 
     # def shoot(self, nb):  # nb=0 pour le player et nb=1 pour les monstres
     #     if nb == 0:
     #         self.direction_tir = "up"
     #     elif nb == 1:
     #         self.direction_tir = "down"
-
-
-if __name__ == '__main__':
-    player = Player(3, "vaisseau_player.jpg")
-    monster = Monster(lives=3, speed=10, position=[
-        300, Sp_Inv.y_fentre_max])
-    monster.deplacement_monstre()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import classes as cl
 from PIL import Image, ImageTk
+from tkinter import messagebox
 
 
 if __name__ == "__main__":
@@ -8,40 +9,30 @@ if __name__ == "__main__":
     # creation de la page window tkinter
     root = tk.Tk()
     window = cl.main_view(root)
-    window.configure(highlightbackground='yellow', highlightthickness=2)
+    window.configure(highlightbackground="yellow", highlightthickness=2)
     window.pack(side="top", fill="both", expand=True)
     # creation du background
     window.update_idletasks()
-    window.new_background('images/background_space_3.png')
-    root.bind("<Motion>", lambda e: [window.new_background(
-        window.background_image), window.cv.tag_lower('background')])  # changer le bind de cet event !!!!
+    window.new_background("images/background_space_3.png")
+    root.bind(
+        "u",
+        lambda e: [
+            window.new_background(window.background_image),
+            window.cv.tag_lower("background"),
+        ],
+    )  # changer le bind de cet event !!!!
 
-    #####JOUEUR######
-    # creation du joueur
-    player = cl.Player(lives=3, img='images/vaisseau_player.png',
-                       position=[window.sizeX/2, window.sizeY*0.75], canvas=window.cv)
-    player.create(tag='player')
+    world = cl.World(window.cv)
+    world.level_monster(5)
+    window.cv.bind("<Key>", world.player.deplacement_player)
+    window.cv.bind("<space>", world.player.shoot)
+    world.player_shoot()
+    # world.monster_shoot()
 
-    # mouvement du joueur
-    player.speed = 50  # la ca va vite en vrai pour un debut de jeu
-    window.cv.focus_set()  # mise en place du focus pour prendre en compte les events
-    window.cv.bind('<Key>', player.deplacement_player)
-    # creation du shoot de balles
-    window.cv.bind('<space>', player.shoot)
-    # on lui donne un nom pour différencier des monstres plus tard
-    player.name = 'player'
-    player.deplacement_bullet()
-    player.suppr_bullet()
+    world.player.speed = 50  # la ca va vite en vrai pour un debut de jeu
+    # world.monster.speed = 4  # a voir une vitesse adequate
 
-    #####MONSTER#####
-    # creation d'ennemis
-    monster = cl.Monster(1, window.cv, position=[
-                         window.cv.winfo_width()/2, 70], img='images/vaisseau_enemy_3.png')
-    monster.create(tag='monster')
-    window.cv.tag_raise('monster')
-    # deplacement des monstres
-    monster.speed = 4  # a voir une vitesse adequate
-    print(monster.form)
-    monster.deplacement_monstre()
+    #####MORT#####
+    world.dead()
 
     root.mainloop()
